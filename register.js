bacee246720c964f45652cad562b8d7ae9185c79
Tiffany Nguyen <53809720+7iffvny@users.noreply.github.com>
2021-02-25 00:32:05 -0500

    fixed register js

--- register.js
+++ register.js
@@ -2,45 +2,13 @@
 function Validate(txt) {
     txt.value = txt.value.replace(/[^a-zA-Z-'\n\r.]+/g, '');
 }
+
 // validate email
-function email_validate(email)
-{
+function email_validate(email) {
 var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
-
-    if(regMail.test(email) == false)
-    {
-    document.getElementById("status").innerHTML    = "<span class='warning'>Email address is not valid yet.</span>";
-    }
-    else
-    {
-    document.getElementById("status").innerHTML	= "<span class='valid'>Thanks, you have entered a valid Email address!</span>";	
-    }
-}
-// validate date of birth
-function dob_validate(dob)
-{
-var regDOB = /^(\d{1,2})[-\/](\d{1,2})[-\/](\d{4})$/;
-
-    if(regDOB.test(dob) == false)
-    {
-    document.getElementById("statusDOB").innerHTML	= "<span class='warning'>DOB is only used to verify your age.</span>";
-    }
-    else
-    {
-    document.getElementById("statusDOB").innerHTML	= "<span class='valid'>Thanks, you have entered a valid DOB!</span>";	
-    }
-}
-// validate address
-function add_validate(address)
-{
-var regAdd = /^(?=.*\d)[a-zA-Z\s\d\/]+$/;
-  
-    if(regAdd.test(address) == false)
-    {
-    document.getElementById("statusAdd").innerHTML	= "<span class='warning'>Address is not valid yet.</span>";
-    }
-    else
-    {
-    document.getElementById("statusAdd").innerHTML	= "<span class='valid'>Thanks, Address looks valid!</span>";	
+    if (regMail.test(email) == false) {
+    document.getElementById("status").innerHTML = "<span class='warning'>Email address is not valid.</span>"; 
+} else {
+    document.getElementById("status").innerHTML	= "<span class='valid'></span>";	
     }
 }
