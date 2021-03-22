6c57c9b2dc970059c908dcf1844c1d658ffeb5e9
Tiffany Nguyen <53809720+7iffvny@users.noreply.github.com>
2021-03-22 16:10:47 -0400

    updated database

--- database/bookstore.sql
+++ database/bookstore.sql
@@ -2,10 +2,10 @@
 -- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 14, 2021 at 02:44 AM
+-- Host: localhost
+-- Generation Time: Mar 22, 2021 at 09:10 PM
 -- Server version: 10.4.14-MariaDB
--- PHP Version: 7.3.23
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,13 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `address` (
-  `ADDRESS_ID` int(11) NOT NULL,
-  `FIRST_NAME` varchar(255) NOT NULL,
-  `LAST_NAME` varchar(255) NOT NULL,
-  `STREET` varchar(255) NOT NULL,
-  `CITY` varchar(255) NOT NULL,
-  `STATE` varchar(255) NOT NULL,
-  `ZIP` int(5) NOT NULL
+  `ADDRESS_ID` int(11) NOT NULL COMMENT 'Address ID, Auto generated',
+  `FIRST_NAME` varchar(255) NOT NULL COMMENT 'User First Name',
+  `LAST_NAME` varchar(255) NOT NULL COMMENT 'User Last Name',
+  `STREET` varchar(255) NOT NULL COMMENT 'Street of Address',
+  `CITY` varchar(255) NOT NULL COMMENT 'City of Address',
+  `STATE` varchar(255) NOT NULL COMMENT 'State of Address',
+  `ZIP` int(5) NOT NULL COMMENT 'Zip Code of Address'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -44,20 +44,20 @@ CREATE TABLE `address` (
 --
 
 CREATE TABLE `book` (
-  `BOOK_ID` int(11) NOT NULL,
-  `TITLE` varchar(255) NOT NULL,
-  `ISBN` varchar(13) NOT NULL,
-  `AUTHOR_NAME` varchar(255) NOT NULL,
-  `CATEGORIES` varchar(255) NOT NULL,
-  `DESCRIPTION` varchar(255) NOT NULL,
-  `COVER_PICTURE` varchar(1000) NOT NULL,
-  `EDITION` int(11) NOT NULL,
-  `PUBLISHER_NAME` varchar(50) NOT NULL,
-  `PUBLISHER_YEAR` year(4) NOT NULL,
-  `PRICE` double(10,2) NOT NULL,
-  `QUANTITY` int(11) NOT NULL,
-  `KEYWORDS` varchar(255) DEFAULT NULL,
-  `STATUS` int(11) DEFAULT NULL,
+  `BOOK_ID` int(11) NOT NULL COMMENT 'Book ID, Auto generated',
+  `TITLE` varchar(255) NOT NULL COMMENT 'Title of Book',
+  `ISBN` varchar(13) NOT NULL COMMENT '(International Standard Book Number). A unique 13 digit identifier given to each edition of a book',
+  `AUTHOR_NAME` varchar(255) NOT NULL COMMENT 'Name of Author of Book',
+  `CATEGORIES` varchar(255) NOT NULL COMMENT 'Category of Book',
+  `DESCRIPTION` varchar(255) NOT NULL COMMENT 'Description of Book',
+  `COVER_PICTURE` varchar(1000) NOT NULL COMMENT 'Cover Image of Book',
+  `EDITION` int(11) NOT NULL COMMENT 'Edition of Book',
+  `PUBLISHER_NAME` varchar(50) NOT NULL COMMENT 'Name of Publisher of Book',
+  `PUBLISHER_YEAR` year(4) NOT NULL COMMENT 'Publisher Year of Book',
+  `PRICE` double(10,2) NOT NULL COMMENT 'Price of Book',
+  `QUANTITY` int(11) NOT NULL COMMENT 'Amount of Books in the system',
+  `KEYWORDS` varchar(255) DEFAULT NULL COMMENT 'Keywords related to the book',
+  `STATUS` int(11) DEFAULT NULL COMMENT 'Status of book',
   `QUANTITY_ON_HAND` int(11) DEFAULT NULL,
   `MINIMUM_THRESHOLD` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -69,8 +69,8 @@ CREATE TABLE `book` (
 --
 
 CREATE TABLE `cart` (
-  `CART_ID` int(11) NOT NULL,
-  `USER_ID` int(11) NOT NULL
+  `CART_ID` int(11) NOT NULL COMMENT 'Cart ID, Auto generated',
+  `USER_ID` int(11) NOT NULL COMMENT 'User ID that is associated to the cart'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -80,9 +80,9 @@ CREATE TABLE `cart` (
 --
 
 CREATE TABLE `cart_item` (
-  `CART_ID` int(11) NOT NULL,
-  `BOOK_ID` int(11) NOT NULL,
-  `QUANTITY` int(11) NOT NULL
+  `CART_ID` int(11) NOT NULL COMMENT 'Cart ID that is associated to the book in cart',
+  `BOOK_ID` int(11) NOT NULL COMMENT 'Book ID that is associated to the cart item',
+  `QUANTITY` int(11) NOT NULL COMMENT 'Amount of item for the given cart item'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -92,15 +92,15 @@ CREATE TABLE `cart_item` (
 --
 
 CREATE TABLE `order` (
-  `ORDER_ID` int(11) NOT NULL,
-  `USER_ID` int(11) NOT NULL,
-  `PAYMENT_ID` int(11) NOT NULL,
-  `SUBTOTAL` double(10,2) NOT NULL,
-  `TOTAL_TAX` double(10,2) NOT NULL,
-  `GRAND_TOTAL` double(10,2) NOT NULL,
-  `PROMO_ID` int(11) NOT NULL,
-  `ORDER_DATE_TIME` datetime NOT NULL,
-  `SHIPPING_TYPE` int(11) NOT NULL
+  `ORDER_ID` int(11) NOT NULL COMMENT 'Order ID, auto generated',
+  `USER_ID` int(11) NOT NULL COMMENT 'User ID that is associated to the order',
+  `PAYMENT_ID` int(11) NOT NULL COMMENT 'Payment ID that is associated to the order',
+  `SUBTOTAL` double(10,2) NOT NULL COMMENT 'The amount of the order before taxes',
+  `TOTAL_TAX` double(10,2) NOT NULL COMMENT 'The tax calculated based on the sub total of the order',
+  `GRAND_TOTAL` double(10,2) NOT NULL COMMENT 'The total of the order including taxes',
+  `PROMO_ID` int(11) NOT NULL COMMENT 'Promo ID that is associated to the order',
+  `ORDER_DATE_TIME` datetime NOT NULL COMMENT 'The data and time of the order',
+  `SHIPPING_TYPE` int(11) NOT NULL COMMENT 'The type of shipping that is associated to the order'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -110,9 +110,9 @@ CREATE TABLE `order` (
 --
 
 CREATE TABLE `order_item` (
-  `ORDER_ID` int(11) NOT NULL,
-  `BOOK_ID` int(11) NOT NULL,
-  `QUANTITY` int(11) NOT NULL
+  `ORDER_ID` int(11) NOT NULL COMMENT 'Order ID, auto generated',
+  `BOOK_ID` int(11) NOT NULL COMMENT 'Book ID associated to the order item',
+  `QUANTITY` int(11) NOT NULL COMMENT 'Amount of Book for given item'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -122,13 +122,13 @@ CREATE TABLE `order_item` (
 --
 
 CREATE TABLE `payment_card` (
-  `PAYMENT_ID` int(11) NOT NULL,
-  `CARD_NUM` bigint(16) NOT NULL,
-  `USER_ID` int(11) NOT NULL,
-  `BILLING_ADDRESS` int(11) NOT NULL,
-  `TYPE` varchar(255) NOT NULL,
-  `EXP_DATE` date NOT NULL,
-  `CCV` int(3) NOT NULL
+  `PAYMENT_ID` int(11) NOT NULL COMMENT 'Payment ID, auto generated',
+  `CARD_NUM` bigint(16) NOT NULL COMMENT 'Card Number',
+  `USER_ID` int(11) NOT NULL COMMENT 'User ID associated to the payment method',
+  `BILLING_ADDRESS` int(11) NOT NULL COMMENT 'Billing Address for the payment method',
+  `TYPE` varchar(255) NOT NULL COMMENT 'Type of Card Payment',
+  `EXP_DATE` date NOT NULL COMMENT 'Expiration Date for Card',
+  `CVV` int(3) NOT NULL COMMENT 'Card Verification Value'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -138,10 +138,10 @@ CREATE TABLE `payment_card` (
 --
 
 CREATE TABLE `promotion` (
-  `PROMO_ID` int(11) NOT NULL,
-  `START_DATE` date NOT NULL,
-  `END_DATE` date NOT NULL,
-  `DISCOUNT` double(10,2) NOT NULL
+  `PROMO_ID` int(11) NOT NULL COMMENT 'Promo ID, auto generated',
+  `START_DATE` date NOT NULL COMMENT 'Promotion Start Date',
+  `END_DATE` date NOT NULL COMMENT 'Promotion End Date',
+  `DISCOUNT` double(10,2) NOT NULL COMMENT 'Amount of Discount for Promotion'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -151,10 +151,10 @@ CREATE TABLE `promotion` (
 --
 
 CREATE TABLE `shipping_type` (
-  `SHIPPING_ID` int(11) NOT NULL,
-  `SHIPPING_NAME` varchar(255) NOT NULL,
-  `SHIPPING_COST` double(10,2) NOT NULL,
-  `SHIPPING_SPEED` varchar(255) NOT NULL
+  `SHIPPING_ID` int(11) NOT NULL COMMENT 'Shipping ID, auto generated',
+  `SHIPPING_NAME` varchar(255) NOT NULL COMMENT 'Shipping Method Name',
+  `SHIPPING_COST` double(10,2) NOT NULL COMMENT 'Cost of Shipping Method',
+  `SHIPPING_SPEED` varchar(255) NOT NULL COMMENT 'Speed for Shipping Method'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -173,17 +173,17 @@ INSERT INTO `shipping_type` (`SHIPPING_ID`, `SHIPPING_NAME`, `SHIPPING_COST`, `S
 --
 
 CREATE TABLE `user` (
-  `USER_ID` int(11) NOT NULL,
-  `USER_NAME` varchar(255) NOT NULL,
-  `EMAIL_ADDRESS` varchar(255) NOT NULL,
-  `PASSWORD` varchar(255) NOT NULL,
-  `FIRST_NAME` varchar(255) NOT NULL,
-  `LAST_NAME` varchar(255) NOT NULL,
-  `PHONE_NUMBER` int(10) DEFAULT NULL,
-  `BIRTHDAY` date DEFAULT NULL,
-  `USER_STATUS` int(2) NOT NULL DEFAULT 2,
-  `USER_TYPE` int(2) NOT NULL DEFAULT 2,
-  `SHIPPING_ADDRESS` int(11) DEFAULT NULL
+  `USER_ID` int(11) NOT NULL COMMENT 'User ID, auto generated',
+  `FIRST_NAME` varchar(255) NOT NULL COMMENT 'First Name of User',
+  `LAST_NAME` varchar(255) NOT NULL COMMENT 'Last Name of User',
+  `PHONE_NUMBER` int(10) DEFAULT NULL COMMENT 'Phone Number of User',
+  `EMAIL_ADDRESS` varchar(255) NOT NULL COMMENT 'Email Address of User',
+  `USER_NAME` varchar(255) NOT NULL COMMENT 'Username of User',
+  `PASSWORD` varchar(255) NOT NULL COMMENT 'Password of User',
+  `BIRTHDAY` date DEFAULT NULL COMMENT 'Birthday of User',
+  `USER_STATUS` int(2) NOT NULL DEFAULT 2 COMMENT 'Status of User',
+  `USER_TYPE` int(2) NOT NULL DEFAULT 2 COMMENT 'Type of User',
+  `SHIPPING_ADDRESS` int(11) DEFAULT NULL COMMENT 'Shipping Address of User'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -193,8 +193,8 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `user_status` (
-  `STATUS_ID` int(2) NOT NULL,
-  `STATUS_NAME` varchar(255) NOT NULL
+  `STATUS_ID` int(2) NOT NULL COMMENT 'Status ID, auto generated',
+  `STATUS_NAME` varchar(255) NOT NULL COMMENT 'Status of User'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -213,8 +213,8 @@ INSERT INTO `user_status` (`STATUS_ID`, `STATUS_NAME`) VALUES
 --
 
 CREATE TABLE `user_type` (
-  `TYPE_ID` int(2) NOT NULL,
-  `TYPE_NAME` varchar(255) NOT NULL
+  `TYPE_ID` int(2) NOT NULL COMMENT 'Type ID, auto generated',
+  `TYPE_NAME` varchar(255) NOT NULL COMMENT 'Type of User, 1 for Admin, 2 for Customer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -323,43 +323,43 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT for table `address`
 --
 ALTER TABLE `address`
-  MODIFY `ADDRESS_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ADDRESS_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Address ID, Auto generated';
 
 --
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
-  MODIFY `BOOK_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `BOOK_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Book ID, Auto generated';
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `CART_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `CART_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Cart ID, Auto generated';
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `ORDER_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ORDER_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Order ID, auto generated';
 
 --
 -- AUTO_INCREMENT for table `payment_card`
 --
 ALTER TABLE `payment_card`
-  MODIFY `PAYMENT_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `PAYMENT_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Payment ID, auto generated';
 
 --
 -- AUTO_INCREMENT for table `promotion`
 --
 ALTER TABLE `promotion`
-  MODIFY `PROMO_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `PROMO_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Promo ID, auto generated';
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'User ID, auto generated';
 
 --
 -- Constraints for dumped tables
